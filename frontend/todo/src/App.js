51f6c97112f736354393049b24fe21ee2866dd14
Felipe Daniel Nerling <felipednerling@gmail.com>
2024-07-31 23:04:11 -0300

    Inicio do frontend - criado o app react

---
+++ frontend/todo/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <header className="App-header">
+       <p>APP</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
